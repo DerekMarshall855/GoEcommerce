20af98f97e50084fd4033fe1d9f03e0efcc2034f
DerekMarshall855 <derekmarshall855@gmail.com>
2022-05-11 15:33:32 -0400

    Fix errors, add imports, add more bson defs

--- controllers/dynamic.controller.go
+++ controllers/dynamic.controller.go
@@ -1,21 +1,135 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"GoEcommerceApp/database"
+	"GoEcommerceApp/models"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var UserCollection *mongo.Collection = database.UserData(database.Client, "Users")
+var ProductCollection *mongo.Collection = database.ProductData(database.Client, "Products")
+var Validate = validator.New()
 
 func HashPassword(password string) string {
 	return ""
 }
 
-func VerifyPassword(userPassword string, givenPassword string) bool {
-	return false
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+	return false, ""
 }
 
 func SignUp() gin.HandlerFunc {
 
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User Already Exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone number is already in use"})
+			return
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Id = primitive.NewObjectID()
+		user.UserId = user.Id.Hex()
+		token, refreshToken, _ := generate.TokenGenerator(*user.Email, *user.FirstName, *user.LastName, user.UserId) //  Generate will be token alias
+		user.AuthToken = &token
+		user.RefreshToken = &refreshToken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.OrderStatus = make([]models.Order, 0)
+		_, insertErr := UserCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "The user did not get created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusCreated, "Successfully Signed Up!")
+	}
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		var foundUser models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Login or password is incorrect"})
+			return
+		}
+
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+
+		if !PasswordIsValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println(msg)
+			return
+		}
+
+		token, refereshToken, _ := generate.TokenGenerator(*foundUser.Email, *foundUser.FirstName, *foundUser.LastName, *foundUser.UserId)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refereshToken, foundUser.UserId)
+
+		c.JSON(http.StatusFound, foundUser)
+	}
 }
 
 func ProductViewerAdmin() gin.HandlerFunc {
--- models/models.go
+++ models/models.go
@@ -12,11 +12,12 @@ type User struct {
 	LastName       *string            `json:"lastName"  validate:"required,min=2,max=30"`
 	Password       *string            `json:"password"  validate:"required,min=6"`
 	Email          *string            `json:"email"     validate:"email, required"`
+	Phone          *string            `json:"phone" validate:"required"`
 	AuthToken      *string            `json:"authToken"`
 	RefreshToken   *string            `json:"refreshToken"`
 	CreatedAt      time.Time          `json:"createdAt"`
 	UpdatedAt      time.Time          `json:"updatedAt"`
-	UserId         *string            `json:"userId"`
+	UserId         string             `json:"userId"`
 	UserCart       []ProductUser      `json:"userCart" bson:"userCart"`
 	AddressDetails []Address          `json:"addressDetails" bson:"addressDetails"`
 	OrderStatus    []Order            `json:"orderStatus" bson:"orderStatus"`
@@ -32,31 +33,31 @@ type Product struct {
 
 type ProductUser struct {
 	Id     primitive.ObjectID `json:"_id" bson:"_id"`
-	Name   *string            `json:"name"`
-	Price  *uint64            `json:"price"`
-	Rating *uint8             `json:"rating"`
-	Image  *string            `json:"image"`
+	Name   *string            `json:"name" bson:"name"`
+	Price  *uint64            `json:"price" bson:"price"`
+	Rating *uint8             `json:"rating" bson:"rating"`
+	Image  *string            `json:"image" bson:"image"`
 }
 
 type Address struct {
 	Id         primitive.ObjectID `json:"_id" bson:"_id"`
-	Street     *string            `json:"street"`
-	House      *string            `json:"house"`
-	City       *string            `json:"city"`
-	PostalCode *string            `json:"postalCode"`
+	Street     *string            `json:"street" bson:"street"`
+	House      *string            `json:"house" bson:"house"`
+	City       *string            `json:"city" bson:"city"`
+	PostalCode *string            `json:"postalCode" bson:"postalCode"`
 }
 
 type Order struct {
 	Id            primitive.ObjectID `json:"_id" bson:"_id"`
 	Cart          []ProductUser      `json:"cart" bson:"cart"`
-	OrderedAt     time.Time          `json:"orderedAt"`
-	Price         *uint64            `json:"price"`
-	Discount      *int               `json:"discount"`
+	OrderedAt     time.Time          `json:"orderedAt" bson:"orderedAt"`
+	Price         *uint64            `json:"price" bson:"price"`
+	Discount      *int               `json:"discount" bson:"discount"`
 	PaymentMethod Payment            `json:"paymentMethod" bson:"paymentMethod"`
 }
 
 type Payment struct {
 	Id             primitive.ObjectID `json:"_id" bson:"_id"`
-	Digital        bool               `json:"digital"`
-	CashOnDelivery bool               `json:"cashOnDelivery"`
+	Digital        bool               `json:"digital" bson:"digital"`
+	CashOnDelivery bool               `json:"cashOnDelivery" bson:"cashOnDelivery"`
 }
