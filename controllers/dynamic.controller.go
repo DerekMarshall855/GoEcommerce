85a8c3e7ba324d5562fb1b8acc035779f95f9d07
DerekMarshall855 <derekmarshall855@gmail.com>
2022-05-11 14:34:47 -0400

    Setup controller outlines (no functions written, just structure)

---
+++ controllers/dynamic.controller.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+	return ""
+}
+
+func VerifyPassword(userPassword string, givenPassword string) bool {
+	return false
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+
+}
--- models/models.go
+++ models/models.go
@@ -9,9 +9,9 @@ import (
 type User struct {
 	Id             primitive.ObjectID `json:"_id" bson:"_id"`
 	FirstName      *string            `json:"firstName" validate:"required,min=2,max=30"`
-	LastName       *string            `json:"lastName" validate:"required,min=2,max=30"`
-	Password       *string            `json:"password" validate:"required,min=6"`
-	Email          *string            `json:"email" validate:"email, required"`
+	LastName       *string            `json:"lastName"  validate:"required,min=2,max=30"`
+	Password       *string            `json:"password"  validate:"required,min=6"`
+	Email          *string            `json:"email"     validate:"email, required"`
 	AuthToken      *string            `json:"authToken"`
 	RefreshToken   *string            `json:"refreshToken"`
 	CreatedAt      time.Time          `json:"createdAt"`
